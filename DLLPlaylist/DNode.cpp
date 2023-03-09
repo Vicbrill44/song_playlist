cf5094961b356df0d9e20cd5e359c5f1e990b3ca
Vicbrill44 <96606433+Vicbrill44@users.noreply.github.com>
2023-03-08 23:01:28 -0500

    first commit

---
+++ DLLPlaylist/DNode.cpp
@@ -0,0 +1,17 @@
+/*
+ * DNode.cpp
+ *
+ *  Created on: Mar 8, 2023
+ *      Author: victo
+ */
+
+#include "DNode.hpp"
+
+DNode::DNode(){
+
+}
+DNode::DNode(string s, string a, int lenmin, int lensec){
+
+}
+
+
